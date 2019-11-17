cc57bb7ec38dac637436c2f7bd37a45d4d7ff2ff
OmegaFusion <39763713+OmegaFusionCode@users.noreply.github.com>
2019-11-17 03:51:26 +0000

    Level now starts offscreen

--- dev/leveltest.py
+++ dev/leveltest.py
@@ -14,7 +14,7 @@ from levelmaker import LevelMaker
 
 
 FRAMERATE = 60
-INITIAL_X = 400
+INITIAL_X = 1800
 
 
 def gameloop(func):
@@ -54,7 +54,7 @@ class GameWindow(object):
 
         player = Character(self.screen, self.space, self.entities, (100, 600))
         self.entities.append(player)
-        self.entities.append(Floor(self.screen, self.space, self.entities, 0, 2*self.screen.get_width()))
+        self.entities.append(Floor(self.screen, self.space, self.entities, 0, 5*self.screen.get_width()))
 
         # Make the level parts
 
